89a7acf25e4139c6ae3e00518bbc5d064af8c7a4
Xinhao Yuan <xinhaoyuan@gmail.com>
2016-01-20 03:06:26 -0500

    WIP

--- keyboard/hhkb/config.h
+++ keyboard/hhkb/config.h
@@ -37,7 +37,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 /* key combination for command */
-#define IS_COMMAND() (keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT))) 
+// #define IS_COMMAND() (keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)))
+#define IS_COMMAND() (0)
 
 
 /* period of tapping(ms) */
--- keyboard/hhkb/keymap_jp_xinhao.c
+++ keyboard/hhkb/keymap_jp_xinhao.c
@@ -14,9 +14,9 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
 #endif
     /* Layer 0: Default Layer */
     [0] =
-    KEYMAP_JP(ESC ,1   ,2   ,3   ,4   ,5   ,6   ,7   ,8   ,9   ,0   ,MINS,EQL ,GRV ,BSPC, \
-              TAB   ,Q   ,W   ,E   ,R   ,T   ,Y   ,U   ,I   ,O   ,P   ,LBRC,RBRC,         \
-              LCTL   ,A   ,S   ,D   ,F   ,G   ,H   ,J   ,K   ,L   ,SCLN,QUOT,BSLS,ENT, \
+    KEYMAP_JP(ESC ,1   ,2   ,3   ,4   ,5   ,6   ,7   ,8   ,9   ,0   ,MINS,EQL ,GRV ,BSPC,\
+              TAB   ,Q   ,W   ,E   ,R   ,T   ,Y   ,U   ,I   ,O   ,P   ,LBRC,RBRC,        \
+              LCTL   ,A   ,S   ,D   ,F   ,G   ,H   ,J   ,K   ,L   ,SCLN,QUOT,BSLS,ENT ,  \
               LSFT    ,Z   ,X   ,C   ,V   ,B   ,N   ,M   ,COMM,DOT ,SLSH,RSFT,UP  ,RSFT, \
               FN0 ,FN31,LGUI,LALT,MHEN,    SPC     ,HENK,RCTL,RALT,FN0 , LEFT,DOWN,RGHT),
 
@@ -28,17 +28,17 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * |------------------------------------------------------`    |
      * |      |VoD|VoU|Mut|   |   |  *|  /|Hom|PgU|Lef|Rig|   |    |
      * |-----------------------------------------------------------|
-     * |       |   |   |   |   |   |  +|  -|End|PgD|Dow|   |   |   |
+     * |       |PrT|Pla|NxT|   |   |  +|  -|End|PgD|Dow|   |PgU|   |
      * |-----------------------------------------------------------|
-     * |   ||   |   |   |   |         |   |   |   |   ||   |   |   |
+     * |   ||   |   |   |   |         |   |   |MNU|   ||Hom|PgD|End|
      * `-----------------------------------------------------------'
      */
     [1] =
-    KEYMAP_JP(PWR ,F1  ,F2  ,F3  ,F4  ,F5  ,F6  ,F7  ,F8  ,F9  ,F10 ,F11 ,F12 ,INS ,DEL ,\
+    KEYMAP_JP(TRNS,F1  ,F2  ,F3  ,F4  ,F5  ,F6  ,F7  ,F8  ,F9  ,F10 ,F11 ,F12 ,INS ,DEL ,\
               CAPS  ,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS, UP, TRNS,        \
               TRNS   ,VOLD,VOLU,MUTE,TRNS,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,TRNS,PENT,  \
-              TRNS    ,TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,TRNS,PGUP,TRNS, \
-              TRNS,TRNS,TRNS,TRNS,TRNS,    TRNS    ,TRNS,TRNS,TRNS,TRNS, HOME,PGDN,END),
+              TRNS    ,MPRV,MPLY,MNXT,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,TRNS,PGUP,TRNS, \
+              TRNS,TRNS,TRNS,TRNS,TRNS,    TRNS    ,TRNS,RGUI,APP ,TRNS, HOME,PGDN,END),
 
     /* Layer 2: Mouse key mode
      * ,-----------------------------------------------------------.
